e258973ff1e73ca71634de266666c5f218d57961
golsa <chamanigolsa@gmail.com>
2024-05-25 00:52:06 -0700

    add dd_to_cart_view

--- cart/urls.py
+++ cart/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . views import *
+app_name = 'cart'
 urlpatterns = [
 path('', cart_detail_view, name='cart_detail')
 ]    
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,get_object_or_404, redirect
+from . cart import Cart
+from products.models import Product
+from . forms import AddToCartProductForm
 # Create your views here.
 def cart_detail_view(request):
-    return render(request,'cart/cart_detail.html')
+    cart = Cart(request)
+    return render(request,'cart/cart_detail.html', {'cart':cart})
+
+def add_to_cart_view(request,product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product,pk=product_id)
+    form = AddToCartProductForm(request.Post)
+    if form.is_valid():
+        cleaned_data = form.cleaned_data
+        quintity = cleaned_data['quintity']
+        cart.add(product,quintity)
+    return redirect('cart:cart_detail')
